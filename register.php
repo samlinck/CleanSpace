2f786b450246aa942e0eed8706749b513f4c997b
Sam <samlinck@outlook.com>
2019-04-24 12:26:20 +0200

    Finished html and css register

--- register.php
+++ register.php
@@ -9,15 +9,22 @@
     <title>Document</title>
 </head>
 <body>
-    <img src="images/logo_groot.png" alt="logo" class="logoBig">
-    <h1>CleanSpace</h1>
+    <img src="images/logo_groot.png" alt="" class="logoMedium">
+    <h2>CleanSpace</h2>
     <form action="" method="post">
+        <label class="input" for="username">Username</label>
+        <input type="text" name="username" id="username" class="field">
         <label class="input" for="email">Email</label>
         <input type="text" id="email" name="password" class="field">
         <label class="input" for="password">Password</label>
         <input type="password" id="password" name="password" class="field">
-        <input type="submit" value="Login" class="btn">
+        <label class="input" for="repeatPassword">Repeat Password</label>
+        <input type="password" id="repaetpassword" name="repeatpassword" class="field">
+        <label class="checkboxContainer">
+            <input type="checkbox" checked="checked">
+            <span class="checkmark">I agree with the <a class="blue" href="#">terms and conditions</a></span>
+        </label>
+        <input type="submit" value="Register" class="btn">
     </form>
-    <a href="register.php" class="noMember">Not a member yet?</a>
 </body>
 </html>
