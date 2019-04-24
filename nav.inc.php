1288827727c7c66a57286dd562b16917393913a6
ThomasS <thomas.stessels@gmail.com>
2019-04-24 12:50:30 +0200

    added html nav

--- nav.inc.php
+++ nav.inc.php
@@ -1,13 +1,9 @@
 <nav class="navbar">
     <a href="index.php">Home</a>
-    <a href="mylist.php">My Locations</a>
-    <a href="index.php">Profile</a>
-    <a href="mylist.php">Terms</a>
-    <a href="index.php">Settings</a>
-    
-    <form action="" method="get">
-      <input type="text" name="search">
-    </form>
+    <a href="#">My Locations</a>
+    <a href="#">Profile</a>
+    <a href="#">Terms</a>
+    <a href="#">Settings</a>
     
     <a href="logout.php" class="navbar__logout">Hi <?php echo $_SESSION['username']; ?>, logout?</a>
 </nav>
