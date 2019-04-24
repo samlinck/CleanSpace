9544b2dcba1f4e9941d541915ddaa507f4593ff2
Sam <samlinck@outlook.com>
2019-04-24 11:34:43 +0200

    made a loginscreen html and css

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:700,900" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <img src="images/logo_groot.png" alt="logo" class="logoBig">
+    <h1>CleanSpace</h1>
+    <form action="" method="post">
+        <label for="email">Email</label>
+        <input type="text" id="email" name="password" class="field">
+        <label for="">Password</label>
+        <input type="password" id="password" name="password" class="field">
+        <input type="submit" value="Login" class="btn">
+    </form>
+    <a href="register.php" class="noMember">Not a member yet?</a>
+</body>
+</html>
