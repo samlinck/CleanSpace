8a4853025c3e22420dbaf78cf90b2ce593673aa1
ThomasS <thomas.stessels@gmail.com>
2019-05-20 17:14:23 +0200

    new navigation

--- includes/nav.inc.php
+++ includes/nav.inc.php
@@ -1,37 +1,26 @@
 <header>
-  <button class="hamburger">&#9776;</button>
-  <button class="cross">&#735;</button>
+  <div id="navToggle">
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
    <a class="logoSmall" xhref="index.php">CleanSpace</a>
 </header>
 <div class="menu">
   <ul>
-    <a href="#"><li>Home</li></a>
-    <a href="#"><li>My Locations</li></a>
+    <a href="./index.php"><li>Home</li></a>
+    <a href="./locations.php"><li>My Locations</li></a>
     <a href="#"><li>Profile</li></a>
     <a href="#"><li>Terms</li></a>
     <a href="#"><li>Settings</li></a>
-    <a href="#"><li class="menu__logout">Logout</li></a>
+    <a href="./logout.php"><li class="menu__logout">Logout</li></a>
   </ul>
 </div> 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script>
-  $( document ).ready(function() {
-
-      $( ".cross" ).hide();
-      $( ".menu" ).hide();
-      $( ".hamburger" ).click(function() {
-          $( ".menu" ).slideToggle( "slow", function() {
-              $( ".hamburger" ).hide();
-              $( ".cross" ).show();
-          });
-      });
-      
-      $( ".cross" ).click(function() {
-          $( ".menu" ).slideToggle( "slow", function() {
-              $( ".cross" ).hide();
-              $( ".hamburger" ).show();
-          });
-      });
-      
-  });
-</script>
+$( document ).ready(function() {
+    $( "#navToggle" ).click(function() {
+      $(this).toggleClass('open');
+      $( ".menu" ).slideToggle( "slow");
+    });
+});</script>
