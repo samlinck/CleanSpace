4a29b9196790ce49c8ebdb55d6393e31129d9b64
Sam <samlinck@outlook.com>
2019-05-22 10:05:50 +0200

    added an active for making a new issue

--- newIssue.php
+++ newIssue.php
@@ -9,6 +9,46 @@
         $issue->setIssueType($issueType);
         $issue->setSpaceId($spaceId);
 
+        //aanduiding active
+        $activeType = $_GET['issue_type'];
+
+    switch ($activeType) {
+        case "afval":
+            echo '<style type="text/css">
+            #afval {
+                border: 5px solid green;
+                border-radius: 100%;
+            }
+            </style>';
+            break;
+        case "energie":
+        echo '<style type="text/css">
+        #energie {
+            border: 5px solid green;
+            border-radius: 100%;
+        }
+        </style>';
+            break;
+        case "groen":
+        echo '<style type="text/css">
+        #groen {
+            border: 5px solid green;
+            border-radius: 100%;
+        }
+        </style>';
+            break;
+        case "water":
+        echo '<style type="text/css">
+        #water {
+            border: 5px solid green;
+            border-radius: 100%;
+        }
+        </style>';
+            break;
+        // default:
+        //     echo "Your favorite color is neither red, blue, nor green!";
+}
+
         if (!empty($_POST)) {
             $issue->setIssueDesc($_POST['issue']);
             $issue->createIssue();
@@ -37,16 +77,16 @@
             <a href="location.php?location_id=<?php echo $spaceId;?>"><img src="./images/cross.svg" alt=""></a>
             <p>Add issue</p>
             <div class="issue-sorts">
-                <a href="newIssue.php?location_id=<?php echo $spaceId;?>&issue_type=afval">
+                <a href="newIssue.php?location_id=<?php echo $spaceId;?>&issue_type=afval" id="afval">
                     <img src="./images/afval.svg" alt="">
                 </a>
-                <a href="newIssue.php?location_id=<?php echo $spaceId;?>&issue_type=energie">
+                <a href="newIssue.php?location_id=<?php echo $spaceId;?>&issue_type=energie" id="energie">
                     <img src="./images/energie.svg" alt="">
                 </a>
-                <a href="newIssue.php?location_id=<?php echo $spaceId;?>&issue_type=groen">
+                <a href="newIssue.php?location_id=<?php echo $spaceId;?>&issue_type=groen" id="groen">
                     <img src="./images/groen.svg" alt="">
                 </a>
-                <a href="newIssue.php?location_id=<?php echo $spaceId;?>&issue_type=water">
+                <a href="newIssue.php?location_id=<?php echo $spaceId;?>&issue_type=water" id="water">
                     <img src="./images/water.svg" alt="">
                 </a>
             </div>
